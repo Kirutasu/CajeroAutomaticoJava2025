79481653a50c5e2a8830dec2d16aef64ae26e717
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-03-01 18:20:30 +0100

    Metodo transacciones para el ingreso de saldo

--- src/FuncionesCajero/IngresaSaldo.java
+++ src/FuncionesCajero/IngresaSaldo.java
@@ -1,10 +1,18 @@
 package FuncionesCajero;
 
-public class IngresaSaldo extends ClaseAbstractaPrincipal  {
+public class IngresaSaldo extends ClaseAbstractaPrincipal {
 
 
     @Override
     public void Transacciones() {
+        System.out.print("¿Cuanto deseas ingresar?");
+        Deposito(); //se le llama desde aqui al metodo de la clase padre para poder trabajar con ello
 
+        transaccion = getSaldo(); //para poder utilizar el saldo
+        setSaldo(transaccion + deposito); //para establecer el nuevo saldo sumando el previo mas lo que ingresa el user
+        System.out.println("---------------------------------");
+        System.out.println("Ingresaste: " + deposito);
+        System.out.println("Tu saldo actual es: " + getSaldo()); // consulta de la variable saldo
+        System.out.println("---------------------------------");
     }
 }
