47235def7b5342c3413b29178d8061f8ec2d17a8
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-03-01 18:11:25 +0100

    metodo transacciones para Consultar saldo

--- src/FuncionesCajero/ConsultaSaldo.java
+++ src/FuncionesCajero/ConsultaSaldo.java
@@ -5,6 +5,8 @@ public class ConsultaSaldo extends ClaseAbstractaPrincipal  {
 
     @Override
     public void Transacciones() {
-
+        System.out.println("---------------------------------");
+        System.out.println("Tu saldo actual es: " + getSaldo());
+        System.out.println("---------------------------------");
     }
 }
